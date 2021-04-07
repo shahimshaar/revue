75a4faef84c25249f4240118f5be341ba3361e66
Shahim Shaar <shahmshaar5@gmail.com>
2021-04-07 12:16:58 -0500

    thisit

--- server/app.py
+++ server/app.py
@@ -1,53 +1,80 @@
+"""
+GUIDE: This defines the entry point to the flask app.
+"""
+import config
 import os
-from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
+
+from datetime import date
+from flask import Flask, jsonify, send_from_directory
+from flask.json import JSONEncoder
+from flask_cors import CORS
+
+class CustomJSONEncoder(JSONEncoder):
+    """Use ISO 8601 for dates"""
+
+    def default(self, obj):  # noqa: E0202
+        try:
+            if isinstance(obj, date):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
 
 app = Flask(__name__)
+app.json_encoder = CustomJSONEncoder
+app.config["SECRET_KEY"] = config.flask_secret_key
+# GUIDE: Cross-Origin Resource Sharing is a mechanism used by servers to tell the browser which other servers the browser should trust for this site.
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+CORS(app)
 
-app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
 
-db = MongoEngine()
-db.init_app(app)
+# GUIDE: This is how we tell flask to respond to a request on a specific url
+# This particular instance responds to /api/file/filename. The last segment, filename, is turned into a string and passed as an argument to the function
+# The @ syntax is called a function decorator. @app.route() is a function that takes another function as an argument and returns a modified function
+# This is an advanced python feature that can be a lot of fun to waste time with.
+# Flask makes great use of function decorators to attach functions to server responses/behaviors
+# https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.route
 
-@app.route('/')
-def index():
-    return jsonify(
-        status=True,
-        message='Welcome to the Dockerized Flask MongoDB app!'
-    )
+@app.route("/api/file/<string:filename>")
+def images_get(filename):
+    return send_from_directory(config.image_upload_folder, filename)
 
-@app.route('/todo')
-def todo():
-    _todos = db.todo.find()
 
-    item = {}
-    data = []
-    for todo in _todos:
-        item = {
-            'id': str(todo['_id']),
-            'todo': todo['todo']
-        }
-        data.append(item)
+from views.authentication import *  # noqa
+from views.posts import *  # noqa
+from views.subvues import *  # noqa
+from views.users import *  # noqa
 
-    return jsonify(
-        status=True,
-        data=data
-    )
+import errors  # noqa
 
-@app.route('/todo', methods=['POST'])
-def createTodo():
-    data = request.get_json(force=True)
-    item = {
-        'todo': data['todo']
-    }
-    db.todo.insert_one(item)
 
-    return jsonify(
-        status=True,
-        message='To-do saved successfully!'
-    ), 201
+# GUIDE: here are functions to respond to various server errors. Note the use of decorators.
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({
+        "error": "API endpoint not found"
+    }), 404
 
-# python main running app, when you do flask run
+
+@app.errorhandler(500)
+@app.errorhandler(405)
+def internal_server_error(e):
+    return jsonify({
+        "error": "Internal server error"
+    }), 500
+
+
+@app.errorhandler(413)
+def request_entity_too_large(e):
+    return jsonify({
+        "error": "To large (max. 1 MB)"
+    }), 413
+
+
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
     ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
